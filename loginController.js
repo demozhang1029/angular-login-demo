57c68c4dedb9d1c598e7e291c524eb3c5997492c
xinzhang <xinzhang@thoughtworks.com>
2016-12-22 10:39:26 +0800

    refactor magic number

--- loginController.js
+++ loginController.js
@@ -1,6 +1,10 @@
 var app = angular.module('loginModule', ['ngResource']);
 
 app.controller('loginCtrl', ($scope, $resource) => {
+
+	var LOGIN_SUCCESS = 0;
+	var CONNECT_FAIL = -1;
+
 	$scope.login = () => {
 		if ($scope.currentLocation == 'Select Location') {
 			$scope.result = 'Please select an item in the list'
@@ -9,14 +13,14 @@ app.controller('loginCtrl', ($scope, $resource) => {
 		var LoginConn = $resource('http://localhost:8080/login');
 		LoginConn.save({}, {'username':$scope.username, 'password':$scope.password},
 			function(response){
-				if (response.status == 0) {
+				if (response.status == LOGIN_SUCCESS) {
 					$scope.result = 'Login Success';
 				}else {
 					$scope.result = 'You are not authenticated or your session expired. Please login.';
 				}
 			},
 			function(response){
-				if (response.status == -1) {
+				if (response.status == CONNECT_FAIL) {
 					$scope.result = 'Can\'t connect to service';
 				}
 			});
